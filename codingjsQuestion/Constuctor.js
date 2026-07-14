474c60cd8561395e3cb91ff2c333bf501ed019d7
Vishal  Mishra <123309972+perfectcse@users.noreply.github.com>
2026-07-14 21:07:42 +0530

    update

--- codingjsQuestion/Constuctor.js
+++
@@ -1,4 +0,0 @@
-function name (a,b){
-    return 5+8;
-}
-console.log(a,b);
--- codingjsQuestion/Hello.js
+++
@@ -1,8 +0,0 @@
-//Write a function createHelloWorld. It should return a new function that always returns "Hello World".
-
-function createHelloWorld(){
-    return function (){
-        return "Hello World";
-    }
-}
-console.log(createHelloWorld()());
